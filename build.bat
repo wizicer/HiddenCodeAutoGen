c36713c4e746a9bab9b5d81c7db342906346d302
Icer <liangshuangde@163.com>
2016-02-19 10:08:39 +0800

    enhance build script and release

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 @echo off
 rd output /s /q
 md output
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild IcerWPFSmartGen\IcerWPFSmartGen.csproj /t:rebuild /p:configuration=Release
-copy IcerWPFSmartGen\bin\Release\*.dll output\*.dll
-copy UnitTests\AutoGen*.cs output\*.cs
+path %PATH%;C:\Program Files\MSBuild\14.0\Bin;
+path %PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin;
 
+
+MSBuild Generator\Generator.csproj /t:rebuild /p:configuration=Release
+copy Generator\bin\Release\HiddenCodeAutoGenerator.vsix
+
+pause
